1163cd58e96cac975cbc6172578415782ef2cfda
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-26 15:02:46 -0300

    chore:page404

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
+import Info from "./pages/Info";
+import NotFound from './components/NotFound';
 
 //components
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
@@ -23,8 +26,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* 6 - nested route */}
+          <Route path="/products/:id/info" element={<Info />} />
           {/*Rota dinâmica */}
           <Route path="/products/:id" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
