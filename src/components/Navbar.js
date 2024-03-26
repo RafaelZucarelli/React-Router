4070bba4f7be2a4f4c8c3a789f08eb15e1670a14
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-26 15:14:42 -0300

    chore:NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,29 @@
 //links com react router
 import React from 'react'
 import "./Navbar.css"
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">About</Link> */}
+
+      {/*<NavLink to="/">Home</NavLink>
+      <NavLink to="/about">Sobre</NavLink>*/}
+
+<NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/about"
+      >
+        Sobre
+      </NavLink>
     </nav>
   )
 }
